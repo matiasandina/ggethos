45d53990b29ea8c2c307119f406403271eba8250
David Wilkins <david@wilkox.org>
2022-10-30 10:58:45 +1030

    Add more examples

--- examples.R
+++ examples.R
@@ -36,6 +36,16 @@ ggplot(wombats_duration_dt, aes(x = start_dt, xend = end_dt, y = wombat, colour 
   geom_ethogram() +
   facet_wrap(wombat ~ trial, scales = "free")
 
+
+load_all()
+ggplot(wombats_frame, aes(y=wombat)) + geom_ethogram(color="red") + facet_wrap(~trial)
+
+load_all()
+ggplot(wombats_frame, aes(y=wombat)) + geom_ethogram(aes(color=wombat)) + facet_wrap(~trial)
+
+load_all()
+ggplot(wombats_frame, aes(y=wombat)) + geom_ethogram(aes(color=behaviour)) + facet_wrap(~trial)
+
 # This produces weird behavior, am I not understanding the syntax  ?
 # By the way, none of the examples have y=behaviour
 load_all()
@@ -102,3 +112,134 @@ load_all()
 # Again, I think the problem is that the split is on behavior because the y is behavior, but that changes the temporal order when it shouldn't
 wombats_seconds %>% group_by(wombat) %>% mutate(time = 1:n()) %>%  ggplot(aes(y = behaviour)) + geom_ethogram() + facet_wrap(~wombat)
 
+
+## More examples
+
+# does not work as expected
+ggplot(wombats_ordered, aes(y=wombat)) + 
+  geom_ethogram()
+
+ggplot(wombats_ordered, aes(y=wombat)) + 
+  geom_ethogram(color="salmon")
+
+# expected result is this, but one color
+ggplot(wombats_ordered, aes(y=wombat, color=behaviour)) + 
+  geom_ethogram()
+
+# does not work as expected
+ggplot(wombats_ordered, aes(y=behaviour))+
+  geom_ethogram() +
+  facet_wrap(~wombat) 
+
+# does not work as expected
+# NAs ar e filtered when color is specified, but the Y axis still has them 
+ggplot(wombats_ordered, aes(y=behaviour, color=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(~wombat)
+
+# x axis is correct, multiple bins
+# looks like there's overlap in behaviors ?
+ggplot(wombats_frame, aes(x=frame, y=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(~wombat)
+
+# I guess I might be using the wrong x variable
+ggplot(wombats_seconds, aes(x=seconds, y=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(~wombat, scales="free")
+
+# x axis is correct, NAs are filtered but still on y axis 
+# looks like there's overlap in behaviors ?
+ggplot(wombats_frame, aes(x=frame, y=behaviour, color=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(~wombat)
+
+# this one is doing weird things
+ggplot(wombats_seconds, aes(x=seconds, y=behaviour, color=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(~wombat, scales="free")
+
+# expected output, although many bins
+ggplot(wombats_frame, aes(x=frame, y=behaviour))+
+   geom_ethogram() + 
+  facet_wrap(trial~wombat)
+
+ggplot(wombats_seconds, aes(x=seconds, y=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(trial~wombat, scales="free")
+
+  
+# Produces overlapping intervals when lumping behaviors
+ggplot(wombats_frame, aes(x=frame, y=behaviour, color=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(trial~wombat)
+
+ggplot(wombats_seconds, aes(x=seconds, y=behaviour, color=behaviour))+
+  geom_ethogram() + 
+  facet_wrap(trial~wombat, scales="free")
+
+# does not work as expected
+ggplot(wombats_frame, aes(x = frame, y = wombat)) +
+  geom_ethogram() +
+  facet_wrap(~ trial)
+# expected
+ggplot(wombats_frame, aes(x = frame, y = wombat, color=behaviour)) +
+  geom_ethogram() +
+  facet_wrap(~ trial)
+
+# not the same output as the colored one
+# is it just filtering of NAs ?
+ggplot(wombats_seconds, aes(x = seconds, y = wombat)) +
+  geom_ethogram() +
+  facet_wrap(~ trial)
+
+ggplot(wombats_seconds, aes(x = seconds, y = wombat, colour = behaviour)) +
+  geom_ethogram() +
+  facet_wrap(~ trial)
+
+# lumps all behaviors and creates overlap
+ggplot(wombats_seconds, aes(x = seconds, y = behaviour, color=behaviour)) +
+  geom_ethogram()
+
+# expected output
+ggplot(wombats_seconds, aes(x = seconds, y = behaviour)) +
+  geom_ethogram()
+
+# does not produce expected output
+# is it just the filtering of NAs ?
+ggplot(wombats_seconds, aes(x = seconds, y = wombat)) +
+  geom_ethogram(align_trials = FALSE) 
+
+ggplot(wombats_seconds, aes(x = seconds, y = wombat, color=behaviour)) +
+  geom_ethogram(align_trials = FALSE) 
+
+
+# issues with scale here
+# unlikely people try to plot things like these
+ggplot(wombats_dt, aes(x = dt, y = wombat, colour = behaviour)) +
+  geom_ethogram() +
+  facet_wrap(~ trial , scales = "free")
+
+# only visible when adding wombat to the facet_wrap
+# I think it's unintuitive
+ggplot(wombats_dt, aes(x = dt, y = wombat, colour = behaviour)) +
+  geom_ethogram() +
+  facet_wrap(wombat ~ trial , scales = "free")
+
+ggplot(wombats_duration, aes(x = start_seconds, xend = end_seconds, y = wombat, colour = behaviour)) +
+  geom_ethogram() +
+  facet_wrap(~ trial)
+
+# I'm not sure but the behavior of align trials might be wrong here
+ggplot(wombats_duration, aes(x = start_seconds, xend = end_seconds, y = wombat, colour = behaviour)) +
+  geom_ethogram(align_trials = TRUE) +
+  facet_wrap(~ trial)
+
+ggplot(wombats_duration, aes(x = start_seconds, xend = end_seconds, y = trial, colour = behaviour)) +
+  geom_ethogram(align_trials = TRUE) +
+  facet_wrap(~ wombat)
+
+# also not sure if it's getting rid of spaces that were previously NAs 
+ggplot(wombats_duration, aes(x = start_seconds, xend = end_seconds, y = trial, colour = behaviour)) +
+  geom_ethogram(align_trials = FALSE) +
+  facet_wrap(~ wombat)
