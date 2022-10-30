59a107097080ecc71c1ff1a6c169de2a43713de0
matiasandina <matiasandina@gmail.com>
2022-10-30 14:05:36 -0400

    changing example data

--- generate_sample_data.R
+++ generate_sample_data.R
@@ -18,51 +18,54 @@ generate_behaviours <- function(n) {
 }
 
 # Behaviours are implied to be presented in order with a fixed interval between
-# observations. NA represents an observation where there was no behaviour
-# observed
+# observations, as if they were recorded from a film. NA represents a
+# frame where there was no behaviour observed/assigned
 wombats_ordered <- tibble(wombat = c("jerry", "pomelo", "speedy", "gimli")) %>%
   mutate(trial = 1:4) %>%
   expand(wombat, trial) %>%
   mutate(n_observations = round(rnorm(16, mean = 50, sd = 15))) %>%
-  mutate(behaviour = lapply(n_observations, generate_behaviours)) %>%
+  mutate(behaviour = lapply(n_observations, generate_behaviours),
+         frame = purrr::map(n_observations, ~1:.x)) %>%
   select(-n_observations) %>%
-  unnest(behaviour)
-use_data(wombats_ordered, overwrite = TRUE)
-
-# Behaviours are recorded for each consecutive frame of a film. NA represents a
-# frame where there was no behaviour observed
-wombats_frame <- wombats_ordered %>%
-  group_by(wombat, trial) %>%
-  mutate(frame = 1:n()) %>%
-  ungroup() %>%
-  select(wombat, trial, frame, behaviour)
-use_data(wombats_frame, overwrite = TRUE)
+  unnest(c(behaviour, frame))
 
 # Behaviours are recorded at regular 5-second intervals, with 'seconds'
-# representing seconds elapsed since the start of the experiment. NA represents
-# an observation where there was no behaviour observed. 'seconds' is measured
-# relative to the start of the experiment rather than each trial to allow
+# representing seconds elapsed since the start of the experiment.
+# 'seconds' is measured relative to the start of the experiment rather than each trial to allow
 # testing of the 'align_trials' argument
-wombats_seconds <- wombats_ordered %>%
-  mutate(seconds = seq(from = 0, by = 5, length.out = n())) %>%
-  select(wombat, trial, seconds, behaviour)
-use_data(wombats_seconds, overwrite = TRUE)
+sampling_period <- 5
+wombats_ordered <- wombats_ordered %>%
+  group_by(wombat) %>%
+  mutate(seconds = seq(from = 0, by = sampling_period, length.out = n()))
 
-# As in wombats_seconds, except that rather then the time of an observation
+# As in `seconds`, except that rather then the time of an observation
 # being given as seconds elapsed since the start of the experiment, it is given
 # as a datetime
-wombats_dt <- wombats_seconds %>%
-  nest(data = c(seconds, behaviour)) %>%
-  mutate(start_dt = as_datetime(rnorm(n(), 1577880000, 100000000))) %>%
+wombats_ordered <- wombats_ordered %>%
+  group_by(wombat) %>%
+  nest() %>%
+  mutate(exp_dt = as_datetime(rnorm(n(), 1577880000, 10000000))) %>%
   unnest(data) %>%
-  mutate(dt = start_dt + dseconds(seconds)) %>%
-  select(wombat, trial, dt, behaviour)
-use_data(wombats_dt, overwrite = TRUE)
+  mutate(
+    start_dt = exp_dt + dseconds(seconds),
+    end_dt = lead(start_dt, 1, default=last(start_dt) + sampling_period))
+
+wombats <- wombats_ordered
+usethis::use_data(wombats, overwrite = TRUE)
+
+wombats_summ <-
+wombats_ordered %>%
+  group_by(wombat, trial) %>%
+  summarise(exp_dt = unique(exp_dt),
+            trial_start = first(seconds),
+            trial_end = last(seconds))
 
 # Observed behaviours are recorded with their start and end times, given in
 # seconds since the start of the trial. There are no NA values; if a NA value
 # was to be included it would probably represent missing data
 wombats_duration <- wombats_ordered %>%
+  ungroup() %>%
+  select(wombat, trial, behaviour) %>%
   mutate(behaviour = replace_na(behaviour, "nothing")) %>%
   nest(behaviours = behaviour) %>%
   mutate(behaviours = map(behaviours, ~ .x$behaviour)) %>%
@@ -76,14 +79,16 @@ wombats_duration <- wombats_ordered %>%
   ungroup() %>%
   select(wombat, trial, behaviour, start_seconds, end_seconds) %>%
   filter(! behaviour == "nothing")
-use_data(wombats_duration, overwrite = TRUE)
 
 # As in wombats_duration, except that start and end times are given as datetimes
-wombats_duration_dt <- wombats_duration %>%
-  nest(data = c(behaviour, start_seconds, end_seconds)) %>%
-  mutate(trial_start_dt = as_datetime(rnorm(n(), 1577880000, 100000000))) %>%
-  unnest(data) %>%
-  mutate(start_dt = trial_start_dt + dseconds(start_seconds)) %>%
-  mutate(end_dt = trial_start_dt + dseconds(end_seconds)) %>%
-  select(wombat, trial, behaviour, start_dt, end_dt)
-use_data(wombats_duration_dt, overwrite = TRUE)
+wombats_duration <- wombats_duration %>%
+  left_join(wombats_summ, by=c("wombat", "trial")) %>%
+  mutate(start_seconds = start_seconds + trial_start,
+         end_seconds = ifelse(trial == 1,
+                              end_seconds,
+                              end_seconds+trial_end),
+         start_dt = exp_dt + dseconds(start_seconds),
+         end_dt = exp_dt + dseconds(end_seconds))
+
+usethis::use_data(wombats_duration, overwrite = TRUE)
+
